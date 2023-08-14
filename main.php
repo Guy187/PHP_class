3cc46af05a392de62a96ba8a26a7b5b089f3078b
Guy187 <guylherme699@gmail.com>
2023-08-14 00:30:23 -0300

    php poo

---
+++ main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <?php
+  echo "[Hello word]";
+  ?>
+
+<?php
+class Cachorro{
+    public $nome;
+    public $raca;
+    public $idade;
+
+    public function latir() {
+        echo "Au au!";
+    }
+};
+</body>
+</html>
